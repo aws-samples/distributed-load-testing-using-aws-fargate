bbeb76eceacad74eff0741d29ca4a787a0007d16
Fernando Dingler <fdingler@amazon.com>
2018-11-03 11:43:37 -0700

    Added sample test that indexes documents in elasticsearch

--- examples/elasticsearch/index.test.js
+++ examples/elasticsearch/index.test.js
@@ -2,25 +2,29 @@
 // SPDX-License-Identifier: MIT-0
 const elasticsearch = require('elasticsearch');
 const awsElasticSearch = require('http-aws-es');
+const ChanceJs = require('chance');
+const chance = new ChanceJs();
 
-describe('Elasticsearch GameDay', function() {
+describe('Elasticsearch GameDay', () => {
 
   const elasticSearchUrl = 'https://banana.us-west-2.es.amazonaws.com';
+  const index = 'performance-testing';
   const client = new elasticsearch.Client({
     hosts: [elasticSearchUrl],
     connectionClass: awsElasticSearch,
     apiVersion: "6.3",
   });
 
-  it('check health of cluster', function (done) {
-    return client.cluster.health()
-      .then(data => {
-        console.log('Health=', data);
-        done();
-      })
-      .catch(err => {
-        console.log('Error=', err);
-        done();
-      });
+  it('stores a JSON document', () => {
+    return client.index({
+      index: index,
+      type: 'dummyType',
+      id: chance.hash(),
+      body: {
+        name: chance.first(),
+        created: new Date().getTime(),
+        description: chance.paragraph(),
+      },
+    });
   });
 });
