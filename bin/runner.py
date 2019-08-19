207149d41531fb154dd7bfbe620a5b0da63a3c9d
Christopher Amici <cda210@gmail.com>
2019-08-19 11:52:16 -0400

    Add files via upload
    added batch functionality

--- bin/runner.py
+++ bin/runner.py
@@ -38,6 +38,9 @@ def get_regions_from_environment_variables():
 
 
 def start_distributed_load_test():
+    if int(TASK_COUNT)>50:
+        print('AWS LIMIT OF 50 TASKS AT ONE TIME EXCEEDED. TASK_COUNT MUST BE LESS THAN 50')
+        exit(0)
     run_id = str(uuid.uuid4())
     print('Started new load test with runId = {}'.format(run_id))
 
@@ -45,70 +48,91 @@ def start_distributed_load_test():
     regions = get_regions_from_environment_variables()
 
     for region in regions:
-
         if not region['stackName']:
             continue
 
-        cloud_formation = boto3.client('cloudformation', region_name=region['name'])
-        ecs = boto3.client('ecs', region_name=region['name'])
+        lastPass = int(TASK_COUNT % 10)
+        totalPasses = int(TASK_COUNT / 10) 
+        
+        if  lastPass > 0 and TASK_COUNT != 0:
+            totalPasses = int(totalPasses + 1)
+       
+        if  totalPasses > 1:
+            print('Batching Task Requests in Groups of 10. Total passes = {}'.format(str(totalPasses)))
+        
+        if  lastPass > 0:
+            print('Last batch will contain {} task requests'.format(str(lastPass)))
+        
+        while totalPasses > 0:
+            cloud_formation = boto3.client('cloudformation', region_name=region['name'])
+            ecs = boto3.client('ecs', region_name=region['name']) #boto3 client needs to be initiated in each batch
 
-        print('Describing CloudFormation stack {} in region {}'.format(region['stackName'], region['name']))
-        stacks = cloud_formation.describe_stacks(
-            StackName=region['stackName']
-        )
+            print('Describing CloudFormation stack {} in region {}'.format(region['stackName'], region['name']))
+            stacks = cloud_formation.describe_stacks(
+                StackName=region['stackName']
+            )
 
-        if not stacks['Stacks']:
-            print("CloudFormation stack {} not found in region {}".format(region['stackName'], region['name']))
-            exit(0)
+            if not stacks['Stacks']:
+                print("CloudFormation stack {} not found in region {}".format(region['stackName'], region['name']))
+                exit(0)
 
-        stack = stacks['Stacks'][0]
-        outputs = stack['Outputs']
-        stack_outputs = {}
+            stack = stacks['Stacks'][0]
+            outputs = stack['Outputs']
+            stack_outputs = {}
 
-        print('Extracting cluster values from CloudFormation stack')
-        for output in outputs:
-            stack_outputs[output['OutputKey']] = output['OutputValue']
-
-        print('Scheduling tasks in region {}'.format(region['name']))
-        response = ecs.run_task(
-            cluster=stack_outputs['FargateClusterName'],
-            taskDefinition=stack_outputs['TaskDefinitionArn'],
-            count=TASK_COUNT,
-            startedBy=run_id,
-            group=run_id,
-            launchType='FARGATE',
-            overrides={
-                'containerOverrides': [
-                    {
-                        'name': 'dlt-fargate-task',
-                        'environment': [
-                            {
-                                'name': 'ENDPOINT_UNDER_TEST',
-                                'value': ENDPOINT_UNDER_TEST
-                            },
-                        ],
-                    },
-                ]
-            },
-            networkConfiguration={
-                'awsvpcConfiguration': {
-                    'assignPublicIp': 'ENABLED',
-                    'securityGroups': [stack_outputs['TaskSecurityGroup']],
-                    'subnets': [
-                        stack_outputs['SubnetA'],
-                        stack_outputs['SubnetB'],
-                        stack_outputs['SubnetC']
+            print('Extracting cluster values from CloudFormation stack')
+            for output in outputs:
+                stack_outputs[output['OutputKey']] = output['OutputValue']       
+           
+            currentPass = totalPasses
+            
+            tasksInThisPass = 0           
+            
+            if  currentPass == 1:
+                tasksInThisPass = lastPass
+            
+            if  currentPass > 1:
+                tasksInThisPass = 10
+        
+            response = ecs.run_task(
+                cluster=stack_outputs['FargateClusterName'],
+                taskDefinition=stack_outputs['TaskDefinitionArn'],
+                count=tasksInThisPass,
+                startedBy=run_id,
+                launchType='FARGATE',
+                overrides={
+                    'containerOverrides': [
+                        {
+                            'name': 'dlt-fargate-task',
+                            'environment': [
+                                {
+                                    'name': 'ENDPOINT_UNDER_TEST',
+                                    'value': ENDPOINT_UNDER_TEST
+                                },
+                            ],
+                        },
                     ]
+                },
+                networkConfiguration={
+                    'awsvpcConfiguration': {
+                        'assignPublicIp': 'ENABLED',
+                        'securityGroups': [stack_outputs['TaskSecurityGroup']],
+                        'subnets': [
+                            stack_outputs['SubnetA'],
+                            stack_outputs['SubnetB'],
+                            stack_outputs['SubnetC']
+                        ]
+                    }
                 }
-            }
-        )
+            )
 
-        if not response or response['failures']:
-            print('Failed to schedule tasks')
-            exit(0)
+            if not response or response['failures']:
+                print('Failed to schedule tasks')
+                exit(0)
 
-        for task in response['tasks']:
-            print('Task scheduled {}', task['taskArn'])
+            for task in response['tasks']:
+                print('Task scheduled {}', task['taskArn'])    
+            totalPasses = currentPass   
 
 
 if __name__ == '__main__':
